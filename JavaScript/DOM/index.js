1e5c8b10cad58e9fe8f4c8e3eb79ec24497894f3
Md Rubel Ahamed <141414730+mdrubelahamed@users.noreply.github.com>
2024-03-30 17:20:25 +0530

    update content to organize folder HTML

--- JavaScript/DOM/index.js
+++ JavaScript/DOM/index.js
@@ -1,5 +1,9 @@
-document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Rubel";
+// change text from last ul list
+document.firstElementChild.lastElementChild.querySelector('ul').lastElementChild.innerHTML = 'Rubel';
 
 
 // bg color of btn class into yellow
-document.querySelector("button").style.backgroundColor = "red";
+document.querySelector('button').style.backgroundColor = 'red';
+
+//add class into title
+document.querySelector('#title').classList.add('huge');

7c627ebaa020909a84d334a28fb8a2dc35a17306
Md Rubel Ahamed <141414730+mdrubelahamed@users.noreply.github.com>
2024-04-05 22:29:39 +0530

    add Animation in drum kit and self

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,105 +1,72 @@
 //Add event listner to every button
 // we are using dynamic way to get the drum no so further if we add drum we don't need specify after how many drum we have to stop
-  
+
+// Detecting button press in my html content
   for (let i = 0; i < document.querySelectorAll(".drum").length; i++) { 
     document.querySelectorAll("button")[i].addEventListener("click", function () {
-      var buttonTextContent = this.textContent;
-      switch (buttonTextContent) {
-        case "w":
-          var crash = new Audio("sounds/crash.mp3")
-          crash.play();
-          break;
-
-        case "a":
-          var kick = new Audio("sounds/kick-bass.mp3");
-          kick.play();
-          break;
-
-        case "s":
-          var snare = new Audio("sounds/snare.mp3");
-          snare.play();
-          break;
-
-        case "d":
-          var tom1 = new Audio("sounds/tom-1.mp3");
-          tom1.play();
-          break;
-
-        case "j":
-          var tom2 = new Audio("sounds/tom-2.mp3");
-          tom2.play();
-          break;
-
-        case "k":
-          var tom3 = new Audio("sounds/tom-3.mp3");
-          tom3.play();
-          break;
-          
-        case "l":
-          var tom4 = new Audio("sounds/tom-4.mp3");
-          tom4.play();
-          break;
-
-        default: console.log(buttonTextContent); 
-          break;
-      }
+      let buttonTextContent = this.textContent;
+      makeSound(buttonTextContent);
+      addAnnimation(buttonTextContent);
   });
 }
 
+// Detecting Key press in my html
+document.addEventListener("keydown", function(event) {
+ makeSound(event.key);
+  addAnnimation(event.key);
+});
 
-// Extra things in comment for understandin how i did it then
 
+function addAnnimation(currentKey) {
+  let activeButton = document.querySelector("." + currentKey);
+  // Adding pressed class in to our html buttons
+  activeButton.classList.add("pressed");
 
-// var numberOfButtons = document.querySelectorAll(".drum").length, we can also create an var and use it in the for loop
+  // add delay in code
+  setTimeout(function() {
+    activeButton.classList.remove("pressed"); //my code to be executed after 100 millisecond
+  }, 100);
+}
 
+// Play the sound based on which key get pressed
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      let crash = new Audio("sounds/tom-1.mp3")
+      crash.play();
+      break;
 
-// var audio = new Audio("sounds/tom-1.mp3");
-// audio.play();
+    case "a":
+      let kick = new Audio("sounds/tom-2.mp3");
+      kick.play();
+      break;
 
-/* 
-//Annoymous Function
-//This function
-```function handleClick() {
-  alert("I got Clicked");
-}```
+    case "s":
+      let snare = new Audio("sounds/tom-3.mp3");
+      snare.play();
+      break;
 
-//to this function
-```function() {
-  alert("I got Clicked");
-}```
-*/
+    case "d":
+      let tom1 = new Audio("sounds/tom-4.mp3");
+      tom1.play();
+      break;
 
-// Using if else
-// 
-// 
-// if (this.textContent === "w") {
-//   var audio = new Audio("sounds/crash.mp3");
-//   audio.play();
-// }
-// else if (this.textContent === "a") {
-//   var audio = new Audio("sounds/kick-bass.mp3");
-//   audio.play();
-// }
-// else if (this.textContent === "s") {
-//   var audio = new Audio("sounds/snare.mp3")
-//   audio.play();
-// }
-// else if (this.textContent === "d") {
-//   var audio = new Audio("sounds/tom-1.mp3");
-//   audio.play();
-// }
-// else if (this.textContent === "j") {
-//   var audio = new Audio("sounds/tom-2.mp3");
-//   audio.play();
-// }
-// else if (this.textContent === "k") {
-//   var audio = new Audio("sounds/tom-3.mp3");
-//   audio.play();
-// }
-// else if (this.textContent === "l") {
-//   var audio = new Audio("sounds/tom-4.mp3");
-//   audio.play();
-// }
-// else {
-//   console.log("Wrong Drum Kit Button Pressed");
-// }
+    case "j":
+      let tom2 = new Audio("sounds/crash.mp3");
+      tom2.play();
+      break;
+
+    case "k":
+      let tom3 = new Audio("sounds/kick-bass.mp3");
+      tom3.play();
+      break;
+      
+    case "l":
+      let tom4 = new Audio("sounds/snare.mp3");
+      tom4.play();
+      break;
+
+    default: console.log(key); 
+      break;
+  }
+}
--- Js Starting Practice/object_create.js
+++ Js Starting Practice/object_create.js
@@ -52,3 +52,86 @@ let houseKeeper0 = {
     alert("First Work is done, ready for the second😀")
   }
 */
+
+
+// Create JS Constructor function 
+
+function Friend(name, age, background, currentWork) {
+  this.name = name;
+  this.age = age;
+  this.background = background;
+  this.currentWork = currentWork;
+}
+
+// Object from the function
+let friend1 = new Friend("Rubel", 21, "Science", "Computer Science");
+let friend2 = new Friend("Nasim", 22, "Science", "Computer Application");
+let friend3 = new Friend("Surjya", 22, "Science", "Computer Application");
+let friend4 = new Friend("Anant", 23, "Science", "Computer Application");
+let friend5 = new Friend("Vishal", 22, "Science", "Doing great in Sales Job");
+let friend6 = new Friend("Priyabrato", 22, "Science", "Computer Application");
+let friend7 = new Friend("Suparna", 22, "Science", "Makeup Specialist");
+
+
+// Create Constructor Function
+function EmployeeRecord(eId, name, jobTitle, age, country) {
+  this.eId = eId;
+  this.name = name;
+  this.jobTitle = jobTitle;
+  this.age = age;
+  this.country = country;
+}
+
+
+// Object from the function
+let employee1 = new EmployeeRecord("E01", "Emily Dvais", "Sr. Manage", 35, "USA");
+let employee2 = new EmployeeRecord("E02", "Theodoe Dinh", "Technical Architect", 25, "India");
+let employee3 = new EmployeeRecord("E03", "Luna Sanders", "Director", 40, "Bangladesh");
+let employee4 = new EmployeeRecord("E04", "Penelope Jordan", "Computer Systems Mnageer", 25, "India");
+let employee5 = new EmployeeRecord("E05", "Austin Vo", "Sr. Analyst", 41, "USA");
+let employee6 = new EmployeeRecord("E06", "Joshua Gupta", "Account Represntative", 22, "India");
+let employee7 = new EmployeeRecord("E07", "Luke Martain", "Analyst", 25, "Srilanka");
+
+
+
+// Book Constructor
+function Book(title, author, genre, ISBN) {
+  this.title = title;
+  this.author = author;
+  this.genre = genre;
+  this.ISBN = ISBN;
+  this.workDone = function() {
+    let bookName = prompt("What is your Book name?");
+    alert(bookName + " book object is created");
+
+  }
+}
+
+let atomicHabits = new Book("Atomic Habits", "James Clear", "Self-help", "ISBN001");
+let richDadPoorDad = new Book("Rich Dad Poor Dad", "Robert Kiyosaki", "Personal Finance", "ISBN002");
+let thinkAndGrowRich = new Book("Think And Grow Rich", "Neoplian Hill", "Personal Finance", "ISBN002");
+let theWarrenBuffetWay = new Book("The Warren Buffet Waffet", "Robert Hagstrom", "Stock Market", "ISBN003");
+
+
+
+function RevealFavouriteColor(name, color) {
+  this.name = name;
+  this.color = color;
+  this.reveal = function() {
+    alert(name + "'s favourite color is " + color);
+  }
+}
+
+let rubel = new RevealFavouriteColor("Rubel", "Red");
+
+
+
+function Greet(name) {
+  this.name = name;
+  this.greetMe = function() {
+    alert("Welcome " + name + " We are really greatful to have you with us.");
+  }
+}
+
+let nasim = new Greet("Nasim");
+let vishal = new Greet("Vishal");

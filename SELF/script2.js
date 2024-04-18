7991ec786d3ef68215d31f3073b01d94d0558c48
Md Rubel Ahamed <141414730+mdrubelahamed@users.noreply.github.com>
2024-04-18 19:09:00 +0530

    update script2

--- SELF/script2.js
+++ SELF/script2.js
@@ -6,11 +6,16 @@ const printButton = document.querySelector(".print-page");
 printButton.addEventListener("click", printPage);
 
 function sumOfNaturalNumbers(firstNum, lastNum) {
-  const beforeFirstNum = firstNum - 1;
-  const beforeFirstNumSum = ((beforeFirstNum * (beforeFirstNum + 1)) / 2);
-  const upToLastNumSum = ((lastNum * (lastNum + 1)) / 2);
-  const finalSumOfFirstNumToLastNum = (upToLastNumSum - beforeFirstNumSum);
-  return finalSumOfFirstNumToLastNum;
+  if (firstNum > lastNum) {
+    return `${firstNum} > ${lastNum}. Your first number must be less than or equal to last number.`;
+  }
+  else {
+    const beforeFirstNum = firstNum - 1;
+    const beforeFirstNumSum = ((beforeFirstNum * (beforeFirstNum + 1)) / 2);
+    const upToLastNumSum = ((lastNum * (lastNum + 1)) / 2);
+    const finalSumOfFirstNumToLastNum = (upToLastNumSum - beforeFirstNumSum);
+    return finalSumOfFirstNumToLastNum;
+  }
 }
 
 
@@ -20,5 +25,9 @@ let lastNum = document.querySelector("#num2");
 
 const value = document.getElementById("calculate-sum").addEventListener("click", function() {
   const result = sumOfNaturalNumbers(parseInt(firstNum.value), parseInt(lastNum.value))
-  document.querySelector(".sum-output").textContent = `Your Output is ${result}`;
+
+  if (typeof result == "string"){
+    document.querySelector(".sum-output").textContent = result; }
+    else {
+  document.querySelector(".sum-output").textContent = `Your Output is ${result}`; }
 });
